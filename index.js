08c8a8bbff086cb66956b6c31d87ad6903e6b589
Rushikesh <rushikeshchaudhary11@gmail.com>
2023-09-09 12:41:33 +0530

    created otpgenerator

---
+++ index.js
@@ -0,0 +1,11 @@
+function otpgenerator(){
+    var digit="1234567890";
+    var otp="";
+
+    for(let i=0;i<4;i++)
+    {
+        otp=otp+digit[Math.floor(Math.random()*10)];
+    }
+    document.getElementById("showotp").innerHTML=otp;
+
+}
