a066ac24e8a0f01785c21535d20dfbe0f7c2d842
Rushikesh <rushikeshchaudhary11@gmail.com>
2023-09-10 01:03:53 +0530

    added media queries

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-function otpgenerator(){
-    var digit="1234567890";
-    var otp="";
+function otpgenerator() {
+    var digit = "1234567890";
+    var otp = "";
 
-    for(let i=0;i<4;i++)
-    {
-        otp=otp+digit[Math.floor(Math.random()*10)];
+    for (let i = 0; i < 4; i++) {
+        otp = otp + digit[Math.floor(Math.random() * 10)];
     }
-    document.getElementById("showotp").innerHTML=otp;
+    document.getElementById("showotp").innerHTML = otp;
 
 }
